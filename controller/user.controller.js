3b21c27bb4421bf0555f40e23a9aef80dc00a950
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-26 14:08:46 +0100

    Express-Validator library added

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,9 +1,15 @@
+const { validationResult } = require('express-validator');
+
 const home = (req, res) => {
     res.send('Hello from Controller');
 }
 
 const postUser = (req, res) => {
+    const errors = validationResult(req);
     console.log(req.body);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     res.send('User Post Route');
 }
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { home, postUser } = require('../controller/user.controller');
 const router = express.Router();
+const { check } = require('express-validator');
 
-router.get('/', home);
+router.get('/', 
+[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Please password should at least 5 characters').isLength({ min: 5 }),
+], home);
 
 router.post('/', postUser);
 
