ffa6ce5b272014c1c5c714d69ab32bbcdc73dcce
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-26 09:41:42 +0100

    Our route on Postman from route and controller

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/user.route');
 
 connectDB();
 
 dotenv.config();
+
+app.use('/api/v1/users', userRoutes);
 
 const port = process.env.PORT || 5800;
 
