36ea4c68f9b08dd96167a4cdf0b9324419307e9c
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-27 19:09:11 +0100

    Save user to Database

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,16 +1,33 @@
+const User = require('../models/user.model');
 const { validationResult } = require('express-validator');
 
 const home = (req, res) => {
     res.send('Hello from Controller');
 }
 
-const postUser = (req, res) => {
+const postUser = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password } = req.body;
     
-    return res.send('User Post Route Test');
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            console.log(user);
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        }
+
+        user = new User({ name, email, password });
+        user.save();
+        
+        res.send('User created successfully');
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 module.exports = { home, postUser };
