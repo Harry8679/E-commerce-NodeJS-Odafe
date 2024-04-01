0f44c69ac51a5b3468069e877344f6f73df23bf9
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-01 13:50:26 +0200

    Middleware for Authorization (show the profile of the user connected)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,13 @@
-const authController = (req, res) => {
-    res.send('Auth Route');
+const User = require('../models/user.model');
+
+const authController = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        console.log(user);
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+    }
 }
 
 module.exports = { authController };
